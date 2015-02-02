9f8ed10729c5364be7ac2cf9f8114ca05bd8ddae
Branden Clark <brandenjclark+github@gmail.com>
2015-02-02 00:15:19 -0500

    Adds error handling to 12/24 conversion

--- src/app.js
+++ src/app.js
@@ -42,16 +42,22 @@ function get_shuttle_schedule() {
 }
 
 function conv_12hr_to_24hr(time) {
- var hours = Number(time.match(/^(\d+)/)[1]);
- var minutes = Number(time.match(/:(\d+)/)[1]);
- var AMPM = time.match(/\s(.*)$/)[1];
- if(AMPM == "PM" && hours<12) hours = hours+12;
- if(AMPM == "AM" && hours==12) hours = hours-12;
- var sHours = hours.toString();
- var sMinutes = minutes.toString();
- if(hours<10) sHours = "0" + sHours;
- if(minutes<10) sMinutes = "0" + sMinutes;
- return [hours,minutes];
+  var hours = time.match(/^(\d+)/);
+  if (hours === null) return [0, 0];
+  hours = Number(hours[1]);
+  var minutes = time.match(/:(\d+)/);
+  if (minutes === null) return [0, 0];
+  minutes = Number(minutes[1]);
+  var AMPM = time.match(/\s(.*)$/);
+  if (AMPM === null) return [0, 0];
+  AMPM = AMPM[1];
+  if(AMPM == "PM" && hours<12) hours = hours+12;
+  if(AMPM == "AM" && hours==12) hours = hours-12;
+  var sHours = hours.toString();
+  var sMinutes = minutes.toString();
+  if(hours<10) sHours = "0" + sHours;
+  if(minutes<10) sMinutes = "0" + sMinutes;
+  return [hours,minutes];
 }
 
 function get_next_times(times) {
